9616fb4ca22b1f14f87c6152d1a2c1d3bb79576e
Eduardo Zea <eduzea@gmail.com>
2014-11-14 14:00:35 -0500

    - Fixed bug regarding onclick tab panel event

--- static/js/my/home.js
+++ static/js/my/home.js
@@ -1,10 +1,14 @@
 require(['dojo/dom','dojo/parser',"dijit/registry",  "dojo/on", "dojo/ready", "dojo/domReady!"], 
 function (dom,parser,registry, on, ready) {
     ready(function () { //wait till dom is parsed into dijits
-        parser.instantiate([dom.byId('homeTabContainer')]);
-        var panel = registry.byId('homeTabContainer');   //get dijit from its source dom element
-        on(panel, "Click", function (event) {   //for some reason onClick event doesn't work 
-            saludable.entity_class = panel.selectedChildWidget.id.split('_')[0]; 
+        parser.instantiate([dom.byId('addTabContainer'),dom.byId('showTabContainer') ]);
+        var panelAdd = registry.byId('addTabContainer');   //get dijit from its source dom element
+        on(panelAdd, "Click", function (event) {   //for some reason onClick event doesn't work 
+            saludable.entity_class = panelAdd.selectedChildWidget.id.split('_')[0]; 
+        });
+        var panelShow = registry.byId('showTabContainer');   //get dijit from its source dom element
+        on(panelShow, "Click", function (event) {   //for some reason onClick event doesn't work 
+            saludable.entity_class = panelShow.selectedChildWidget.id.split('_')[0]; 
         });
     });
 });
