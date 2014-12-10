f4bc3d8783cb911930c955ffc3dca0000dc86a1a
Eduardo <eduzea@gmail.com>
2014-12-10 16:18:46 -0500

    Added Config file

---
+++ views/config.py
@@ -0,0 +1,76 @@
+from models.models import *
+NUMERO_DE_FACTURA_INICIAL = 2775
+
+classModels = {'Cliente':Cliente, 
+               'Producto':Producto,
+               'Porcion':Porcion, 
+               'Precio':Precio, 
+               'GrupoDePrecios':GrupoDePrecios, 
+               'Factura':Factura, 
+               'Remision':Remision ,
+               'Empleado':Empleado, 
+               'NumeroFactura':NumeroFactura, 
+               'Venta':Venta,
+               'Proveedor':Proveedor, 
+               'Insumo':Insumo, 
+               'PorcionInsumo':PorcionInsumo, 
+               'Egreso':Egreso}
+keyDefs = {'Cliente':['nombre','negocio'],
+           'Producto':['nombre'], 
+           'Porcion':['valor','unidades'], 
+           'GrupoDePrecios':['nombre'],
+           'Precio':['producto','porcion','grupo'], 
+           'Empleado':['nombre','apellido'], 
+           'Factura':['numero'], 
+           'Remision':['numero'],
+           'Proveedor':['nombre'],
+           'Insumo':['nombre'],
+           'PorcionInsumo':['valor','unidades']}
+
+uiConfig = {'Cliente':[{'id':'nombre','ui':'Nombre', 'required':'true', 'valid':'dijit/form/ValidationTextBox', 'width':'10em'},
+                       {'id':'negocio','ui':'Negocio', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                       {'id':'ciudad','ui':'Ciudad', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                       {'id':'direccion','ui':'Direccion', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                       {'id':'telefono','ui':'Telefono', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                       {'id':'nit','ui':'NIT', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                       {'id':'diasPago','ui':'Dias para pago', 'required':'true', 'valid':'dijit/form/NumberTextBox','width':'10em'},
+                       {'id':'grupoDePrecios','ui':'Grupo de Precios', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'}
+                       ],
+            'Producto':[{'id':'nombre','ui':'Nombre', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'20em'}],
+            'Porcion':[{'id':'valor','ui':'Porcion', 'required':'true', 'valid':'dijit/form/NumberTextBox','width':'10em'},
+                       {'id':'unidades','ui':'Unidades', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'}],
+            'GrupoDePrecios':[{'id':'nombre', 'ui':'Nombre', 'required':'true','valid':'dijit/form/ValidationTextBox','width':'10em'}],
+            'Precio':[{'id':'producto','ui':'Producto'},
+                      {'id':'porcion','ui':'Porcion'},
+                      {'id':'grupo','ui':'Grupo de Precios'},
+                      {'id':'precio','ui':'Precio','required':'true','valid':'dijit/form/NumberTextBox','width':'10em'}
+                      ],
+            'Empleado':[{'id':'nombre', 'ui':'Nombre', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                        {'id':'apellido', 'ui':'Apellido', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'}],
+            'Factura':[{'id':'numero', 'ui':'Numero'},
+                       {'id':'empleado', 'ui':'Empleado'},
+                       {'id':'cliente', 'ui':'Cliente'},
+                       {'id':'fecha', 'ui':'Fecha'},
+                       {'id':'total', 'ui':'Valor'}
+                       ],
+            'Remision':[{'id':'numero', 'ui':'Numero'},
+                       {'id':'empleado', 'ui':'Empleado'},
+                       {'id':'cliente', 'ui':'Cliente'},
+                       {'id':'fecha', 'ui':'Fecha'},
+                       {'id':'total', 'ui':'Valor'}],
+            'Proveedor':[
+                         {'id':'nombre','ui':'Nombre', 'required':'true', 'valid':'dijit/form/ValidationTextBox', 'width':'10em'},
+                         {'id':'ciudad','ui':'Ciudad', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                         {'id':'direccion','ui':'Direccion', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                         {'id':'telefono','ui':'Telefono', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                         {'id':'nit','ui':'NIT', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'},
+                         {'id':'diasPago','ui':'Dias para pago', 'required':'true', 'valid':'dijit/form/NumberTextBox','width':'10em'}
+                         ],
+            'Insumo':[
+                      {'id':'nombre','ui':'Nombre', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'20em'}
+                      ],
+            'PorcionInsumo':[
+                             {'id':'valor','ui':'Porcion', 'required':'true', 'valid':'dijit/form/NumberTextBox','width':'10em'},
+                             {'id':'unidades','ui':'Unidades', 'required':'true', 'valid':'dijit/form/ValidationTextBox','width':'10em'}
+                       ]
+            }
