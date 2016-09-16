da69736fa271c4c6292497dcde1f148cc1e0e12c
Eduardo Zea <eduzea@gmail.com>
2016-09-16 12:41:28 -0500

    Gmail API support

--- cron_handler.py
+++ cron_handler.py
@@ -4,6 +4,7 @@ from google.appengine.api import mail
 from models.models import Factura
 from datetime import datetime
 from oauth2client.contrib.appengine import AppAssertionCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 from httplib2 import Http
 from apiclient.discovery import build
 from apiclient import errors
@@ -19,34 +20,53 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class FacturasVencidas(webapp2.RequestHandler):
     def get(self):
         logging.debug('Running cron job at: ' + str(datetime.today()))
-        facturas = Factura.query(Factura.fechaVencimiento < datetime.today(), 
-                                 Factura.pagada == False).fetch()
-        clienteFacturas = {}
-        for factura in facturas:
-            if factura.cliente.get() is None:
-                logging.debug('Bad Cliente in factura: ' + str(factura.numero))
-                continue
-            if factura.cliente.get().activo == False: continue
-            email = factura.cliente.get().email
-            delta = datetime.today() - factura.fechaVencimiento
-            vencida = {'numero': factura.numero,
-                       'monto':factura.total,
-                       'expedicion': factura.fecha,
-                       'vencimiento':factura.fechaVencimiento.date(),
-                       'vencidos':delta.days,
-                       }
-            cliente = factura.cliente.get().nombre
-            if cliente in clienteFacturas:
-                clienteFacturas[cliente]['facturas'].append(vencida)
-            else:
-                clienteFacturas[cliente]={'email':'eduzea@gmail.com','cliente': cliente, 'fecha':datetime.today().date(), 'facturas':[vencida]}
-                
-        for cliente in clienteFacturas.keys():
-            html = sendEmail(clienteFacturas[cliente])
-            logging.debug('Sending email to: ' + cliente)
-        
-        self.response.write(html)
+#         facturas = Factura.query(Factura.fechaVencimiento < datetime.today(), 
+#                                  Factura.pagada == False).fetch()
+#         clienteFacturas = {}
+#         for factura in facturas:
+#             if factura.cliente.get() is None:
+#                 logging.debug('Bad Cliente in factura: ' + str(factura.numero))
+#                 continue
+#             if factura.cliente.get().activo == False: continue
+#             email = factura.cliente.get().email
+#             delta = datetime.today() - factura.fechaVencimiento
+#             vencida = {'numero': factura.numero,
+#                        'monto':factura.total,
+#                        'expedicion': factura.fecha,
+#                        'vencimiento':factura.fechaVencimiento.date(),
+#                        'vencidos':delta.days,
+#                        }
+#             cliente = factura.cliente.get().nombre
+#             if cliente in clienteFacturas:
+#                 clienteFacturas[cliente]['facturas'].append(vencida)
+#             else:
+#                 clienteFacturas[cliente]={'email':'eduzea@gmail.com','cliente': cliente, 'fecha':datetime.today().date(), 'facturas':[vencida]}
+#                 
+#         html=''
+#         subject= "Facturas vencidas con Salud-Able Foods - " + datetime.today().strftime('%m/%d/%Y')
+#         for cliente in clienteFacturas.keys():
+#             body = messageHTML(clienteFacturas[cliente])
+#             lastMsgBody = sendGmail(clienteFacturas[cliente]['email, subject, body'])
+#             logging.debug('Sending email to: ' + cliente)
+
+        subject= "Facturas vencidas con Salud-Able Foods - " + datetime.today().strftime('%m/%d/%Y')
+        body = "TESTING!"
+        lastMsgBody = sendGmail({'email':'eduzea@gmail.com'},subject,body)
+
+        self.response.write(lastMsgBody)
             
+def sendGmail(recipient,subject, body):
+    #credentials = AppAssertionCredentials('https://mail.google.com/')
+    scopes=['https://www.googleapis.com/auth/gmail.compose']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('saludable-foods-sas-7c9c1120a32e.json', scopes)
+    delegated_credentials = credentials.create_delegated('servicio@salud-able.com')
+    http_auth = delegated_credentials.authorize(Http())
+    service = build('gmail', 'v1', http=http_auth)
+    
+    message = create_message('servicio@salud-able.com', recipient, subject, body)
+    send_message(service, 'servicio@salud-able.com', message)
+    
+    return
 def sendEmail(data):
     message = mail.EmailMessage(
         sender = 'saludable-foods-sas@appspot.gserviceaccount.com',
@@ -78,17 +98,6 @@ def send_message(service, user_id, message):
         return message
     except errors.HttpError, error:
         print 'An error occurred: %s' % error
-
-def sendGmail(data):
-    credentials = AppAssertionCredentials('https://mail.google.com/')
-    delegated_credentials = credentials.create_delegated('servicio@salud-able.com')
-    http_auth = delegated_credentials.authorize(Http())
-    service = build('gmail', 'v1', http=http_auth)
-    subject= "Facturas vencidas con Salud-Able Foods - " + datetime.today().strftime('%m/%d/%Y')
-    message = create_message('servicio@salud-able.com', data.email, subject, messageHTML(data))
-    send_message(service, 'servicio@salud-able.com', message)
-    
-    return
 
 
 app = webapp2.WSGIApplication([('/facturasVencidas', FacturasVencidas)])
